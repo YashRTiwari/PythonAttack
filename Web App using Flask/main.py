9447419f3ba9f512bc91cb817e00f9bdc227d2bc
work.yrt <work.yrt@gmail.com>
2019-03-08 12:25:30 +0530

    Adding rendering html using python

--- Web App using Flask/main.py
+++ Web App using Flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 #Create an object to store flask app instance
 app = Flask(__name__)
@@ -8,12 +8,12 @@ app = Flask(__name__)
 # @app.route('/') --> Decorator
 @app.route('/')
 def home():
-    return "Home Page"
+    return render_template("home.html")
 
 
 @app.route('/about/')
 def about():
-    return "About Page"
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug= True)
