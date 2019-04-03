57f73b13d8e504617d8900a596c31b8607a773a6
work.yrt <work.yrt@gmail.com>
2019-04-03 16:40:56 +0530

    Callbacks to buton, getting string from edittext, putting string to textarea

--- TkinterApp/basefile.py
+++ TkinterApp/basefile.py
@@ -4,13 +4,21 @@ window = Tk()
 
 window.title("Tkinter App")
 
-b1 = Button(window, text = "Execute")
+def kmToMiles():
+    print("Success : "+e1_val.get())
+    miles = float(e1_val.get())*1.6
+    t1.insert(END, miles)
+
+# command  = funct without brackkets
+b1 = Button(window, text = "Execute", command = kmToMiles)
 # TO add widgets to window - 1
 # b1.pack()
 #  2 - more flexibility
 b1.grid(row = 0, column =0)
 
-e1 = Entry(window)
+# Stores value of e1
+e1_val = StringVar()
+e1 = Entry(window, textvariable = e1_val)
 e1.grid(row = 0, column = 1)
 
 t1 = Text(window, height = 1, width = 20)
